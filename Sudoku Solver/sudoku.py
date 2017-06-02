c5f8285f22f9d34bbd898fe3b337b76d8896c80e
rohit-sa <rohitsa.inbox@gmail.com>
2017-06-02 14:59:18 -0700

    ocr pending

--- Sudoku Solver/sudoku.py
+++ Sudoku Solver/sudoku.py
@@ -1,4 +1,5 @@
 import cv2
+import copy
 import numpy as np
 
 def sum_sort(pt1, pt2):
@@ -21,8 +22,6 @@ def main():
 	transformed_sudoku_im = cv2.adaptiveThreshold(transformed_sudoku_im, 255,
 												  cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
 												  cv2.THRESH_BINARY_INV, 15, 2)
-	kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=np.uint8)
-	transformed_sudoku_im = cv2.dilate(transformed_sudoku_im, kernel)
 	connectivity = 4
 	output = cv2.connectedComponentsWithStats(transformed_sudoku_im,
 											connectivity, cv2.CV_32S)
@@ -33,48 +32,63 @@ def main():
 	b_box_x, b_box_y, b_box_w, b_box_h = stats[largest_label, :4]
 	transformed_sudoku_im[labels_im == largest_label] = 255
 	transformed_sudoku_im[labels_im != largest_label] = 0
-	#~ positions = np.where(transformed_sudoku_im == 255)
-	#~ positions = np.asarray(positions).T.tolist()
+	
+	positions = np.where(transformed_sudoku_im == 255)
+	positions = np.asarray(positions).T
+	positions[:,0], positions[:,1] = positions[:,1], positions[:,0].copy()
+	positions = positions.tolist()
 	width_adj = lambda pos: cols - 1 - pos[0]
-	tl_positions = []
-	#~ tr_positions = []
-	for i in range(rows):
-		for j in range(cols):
-			if(transformed_sudoku_im[i,j] == 255):
-				tl_positions.append([j,i])
-				#~ tr_positions.append([i, cols - 1 - j])
-	#~ tl_positions = positions
-	#~ tr_positions = [[width_adj(pos), pos[0]] for pos in positions]
+			
+	tl_positions = positions
 	tr_positions = [[width_adj(pos), pos[1]] for pos in tl_positions]
+	
 	tl_positions.sort(sum_sort)
 	tr_positions.sort(sum_sort)
 	
-	#~ tr = [width_adj(tr_positions[0]), tr_positions[0][0]]
-	#~ bl = [width_adj(tr_positions[-1]), tr_positions[-1][0]]
-	#~ tl = tl_positions[0]
-	#~ br = tl_positions[-1]
-	#~ print(br)
-	tr_positions = [[width_adj(pos), pos[1]] for pos in tr_positions]
-	
-	[tl, tr, br, bl] = [tl_positions[0], tr_positions[0], tl_positions[-1], tr_positions[-1]]
+	tr = [width_adj(tr_positions[0]), tr_positions[0][1]]
+	bl = [width_adj(tr_positions[-1]), tr_positions[-1][1]]
+	tl = tl_positions[0]
+	br = tl_positions[-1]
+
 	rect = np.array([tl, tr, br, bl], dtype = "float32")
 	pts = rect.astype(int)
 	width1 = l2_dist(br, bl)
 	width2 = l2_dist(tr, tl)
 	height1 = l2_dist(br, tr)
 	height2 = l2_dist(tl, bl)
-	#~ print(pts)
 	max_width = max(int(width1), int(width2))
 	max_height = max(int(height1), int(height2))
 	dst = np.array([[0, 0],[max_height - 1, 0],[max_height - 1, max_width - 1], [0, max_width - 1]], dtype = "float32")
 	
 	transform_mat = cv2.getPerspectiveTransform(rect, dst)
-	warp = cv2.warpPerspective(sudoku_im, transform_mat, (max_height,max_width))
-	cv2.polylines(sudoku_im,[pts],True,255)
-	#~ print(tl)
-	#~ cv2.circle(sudoku_im,tuple(tl),3,255)
-	cv2.imshow('sudoku', sudoku_im )
-	cv2.imshow('sudoku1', warp )
+	sudoku_ext_im = cv2.warpPerspective(sudoku_im, transform_mat, (max_height,max_width))
+	sudoku_bin_im = cv2.adaptiveThreshold(sudoku_ext_im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 2)
+	
+	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(max_width/10,1))
+	horizontal = cv2.erode(sudoku_bin_im,kernel,iterations = 1)
+	horizontal = cv2.dilate(horizontal,kernel,iterations = 2)
+	
+	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(1,max_height/10))
+	vertical = cv2.erode(sudoku_bin_im,kernel,iterations = 1)
+	vertical = cv2.dilate(vertical,kernel,iterations = 2)
+	
+	intersection_lines = cv2.bitwise_or(vertical, horizontal)
+	numbers = sudoku_bin_im - intersection_lines
+	numbers_im = copy.deepcopy(numbers)
+	#~ numbers = cv2.medianBlur(numbers,5)
+	_, contours, hierarchy = cv2.findContours(numbers,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	
+	num_area = 0.05*(max_height//10)*(max_width//10)
+	print(num_area)
+	for contour in contours:
+		if(cv2.contourArea(contour) > num_area):
+			x,y,w,h = cv2.boundingRect(contour)
+			img = cv2.rectangle(sudoku_ext_im,(x,y),(x+w,y+h),255,2)
+			#~ cv2.drawContours(sudoku_ext_im, contour, -1, 255, 3)
+	
+	#~ cv2.polylines(sudoku_im,[pts],True,255)
+	cv2.imshow('sudoku', numbers_im )
+	cv2.imshow('sudoku1', sudoku_ext_im )
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 	return
