a97b63e44b6f3a69de4e9a65a7f0e6443886fef4
rohit-sa <rohitsa.inbox@gmail.com>
2017-06-08 16:02:09 -0700

    ocr part

--- Sudoku Solver/sudoku.py
+++ Sudoku Solver/sudoku.py
@@ -64,31 +64,32 @@ def main():
 	sudoku_ext_im = cv2.warpPerspective(sudoku_im, transform_mat, (max_height,max_width))
 	sudoku_bin_im = cv2.adaptiveThreshold(sudoku_ext_im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 2)
 	
-	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(max_width/10,1))
+	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(max_width/9,1))
 	horizontal = cv2.erode(sudoku_bin_im,kernel,iterations = 1)
 	horizontal = cv2.dilate(horizontal,kernel,iterations = 2)
 	
-	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(1,max_height/10))
+	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(1,max_height/9))
 	vertical = cv2.erode(sudoku_bin_im,kernel,iterations = 1)
 	vertical = cv2.dilate(vertical,kernel,iterations = 2)
 	
 	intersection_lines = cv2.bitwise_or(vertical, horizontal)
 	numbers = sudoku_bin_im - intersection_lines
-	numbers_im = copy.deepcopy(numbers)
-	#~ numbers = cv2.medianBlur(numbers,5)
-	_, contours, hierarchy = cv2.findContours(numbers,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	numbers = cv2.medianBlur(numbers,5)
 	
-	num_area = 0.05*(max_height//10)*(max_width//10)
-	print(num_area)
-	for contour in contours:
-		if(cv2.contourArea(contour) > num_area):
-			x,y,w,h = cv2.boundingRect(contour)
-			img = cv2.rectangle(sudoku_ext_im,(x,y),(x+w,y+h),255,2)
-			#~ cv2.drawContours(sudoku_ext_im, contour, -1, 255, 3)
+	cell_height = max_height/9
+	cell_width = max_width/9
 	
+	for i in range(9):
+		#~ print(i*cell_height)
+		for j in range(9):
+			cell = sudoku_ext_im[i*cell_width:(i+1)*cell_width, j*cell_height:(j+1)*cell_height]
+			cv2.rectangle(numbers, (j*cell_height,i*cell_width), ((j+1)*cell_height,(i+1)*cell_width), 255, 2)
+			#~ cv2.imshow('asdf', cell)
+			#~ cv2.waitKey(0)
+			
 	#~ cv2.polylines(sudoku_im,[pts],True,255)
-	cv2.imshow('sudoku', numbers_im )
-	cv2.imshow('sudoku1', sudoku_ext_im )
+	cv2.imshow('sudoku', numbers )
+	#~ cv2.imshow('sudoku1', numbers )
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 	return
---
+++ Sudoku Solver/training/build_model.py
@@ -0,0 +1,91 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import numpy as np
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def model(X):
+	
+	with tf.name_scope('layer1'):
+		with tf.name_scope('weights'):
+			W1 = tf.Variable(tf.truncated_normal([784, 200], stddev = 0.5), name = 'weights')
+		with tf.name_scope('biases'):
+			b1 = tf.Variable(tf.zeros([200]), name = 'biases')
+		
+		h1 = tf.add(tf.matmul(X, W1),b1)
+		h1 = tf.nn.relu(h1)
+		
+	with tf.name_scope('layer2'):
+		with tf.name_scope('weights'):
+			W2 = tf.Variable(tf.truncated_normal([200, 10], stddev = 0.5), name = 'weights')
+		with tf.name_scope('biases'):
+			b2 = tf.Variable(tf.zeros([10]), name = 'biases')
+		
+		out = tf.add(tf.matmul(h1, W2),b2)
+		
+	return out	
+	
+def model1(X):
+	
+	with tf.name_scope('layer1'):
+		
+		
+	with tf.name_scope('layer2'):
+		
+		out = tf.add(tf.matmul(h1, W2),b2)
+		
+	return out		
+
+def main():
+	if os.path.exists('MINST_data/') and len(os.listdir('MINST_data/')) == 4:
+		data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	else:
+		from tensorflow.examples.tutorials.mnist import input_data
+		data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	
+	train_set = data.train
+	valid_set = data.validation
+	test_set = data.test
+	with tf.name_scope('input'):
+		X = tf.placeholder(tf.float32, [None, 784])
+		label = tf.placeholder(tf.float32, [None, 10])
+		
+	Y_logit = model(X)	
+	Y = tf.nn.softmax(Y_logit)
+	
+	with tf.name_scope('loss'):
+		loss = tf.nn.softmax_cross_entropy_with_logits(logits= Y_logit, labels=label)
+		#~ loss = -tf.reduce_sum(label * tf.log(Y))
+		#~ loss = tf.losses.softmax_cross_entropy(onehot_labels= label, logits= Y_logit)
+	with tf.name_scope('evaluation'):
+		correct = tf.equal(tf.argmax(Y,1), tf.argmax(label,1))
+		accuracy = tf.reduce_mean(tf.cast(correct, tf.float32)) 
+	tf.summary.scalar('Accuracy', accuracy)
+	#~ tf.summary.scalar('Loss', loss)
+	optimizer = tf.train.GradientDescentOptimizer(0.003).minimize(loss)
+	
+	merged = tf.summary.merge_all()
+	init_op = tf.global_variables_initializer()
+	
+	with tf.Session() as sess:
+		writer = tf.summary.FileWriter('output', sess.graph)
+		sess.run(init_op)
+		for i in range(1000):
+			batch_X, batch_Y = train_set.next_batch(100)
+			train_data = {X: batch_X, label: batch_Y }
+			summary,_ = sess.run([merged, optimizer], feed_dict = train_data)
+			writer.add_summary(summary, i)
+			a = sess.run([accuracy], feed_dict = train_data)
+		test_data = {X: test_set.images, label: test_set.labels}
+		a, c = sess.run([accuracy, loss], feed_dict = test_data)
+		print(a)
+		writer.close()
+	return
+	
+if __name__ == '__main__':
+	main()
